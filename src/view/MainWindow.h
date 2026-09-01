9a4889bf95491110e8c8a5e86a05e35c7ea3924e
SupCatt <mob.catbee@mail.ru>
2026-09-01 22:38:17 +0400

    Update MainWindow.h

--- src/view/MainWindow.h
+++ src/view/MainWindow.h
@@ -1,8 +1,7 @@
 #pragma once
-
 #include "imgui.h"
 
-// Оригинальные иконки FontAwesome без изменений
+// ─── FontAwesome icons ────────────────────────────────────────────────────────
 #define ICON_FA_HOUSE       "\xef\x80\x95"
 #define ICON_FA_COMPASS     "\xef\x85\x8e"
 #define ICON_FA_SHIRT       "\xef\x95\x93"
@@ -19,14 +18,20 @@
 #define ICON_FA_TIMES       "\xef\x80\x8d"
 #define ICON_FA_DOWNLOAD    "\xef\x80\x99"
 #define ICON_FA_CUBE        "\xef\x86\xb2"
+#define ICON_FA_JAVA        "\xef\x9d\xbe"
+#define ICON_FA_SLIDERS     "\xef\x87\x9e"
+#define ICON_FA_PAINT       "\xef\x87\xbc"
+#define ICON_FA_WINDOW      "\xef\x84\x91"
+#define ICON_FA_SHIELD      "\xef\x84\xa9"
+#define ICON_FA_FOLDER      "\xef\x81\xbb"
+#define ICON_FA_TOGGLE_ON   "\xef\x88\x85"
+#define ICON_FA_TOGGLE_OFF  "\xef\x88\x84"
+#define ICON_FA_SAVE        "\xef\x83\x87"
 
-struct GLFWwindow; // Форвард-декларация, чтобы не тянуть весь glfw3.h
+struct GLFWwindow;
 
-// Функции настройки и отрисовки из оригинального кода
 void SetupFonts(ImGuiIO& io);
 void SetupModrinthStyle();
 void DrawSelectableButton(const char* label, int index, int& current);
 void DrawDownloadToast(const ImGuiIO& io);
-
-// Функция для рендера всего интерфейса (заменит содержимое цикла while)
 void RenderMainWindow(GLFWwindow* window, ImGuiIO& io);
